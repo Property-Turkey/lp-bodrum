b39accf2d028bc7e6b3b0b4d6b5e56f104814868
Osama Kassar <osama@propertyturkey.com>
2023-08-16 16:40:25 +0300

    init

---
+++ locale.php
@@ -0,0 +1,115 @@
+
+    
+    <?php
+
+    $LOCALE = [
+        "en" => [
+            "site_img_alt" => "Property Turkey - Bodrum",
+            "site_motto" => "For life & Investment",
+            "site_title" => "Real estate for sale in Turkey - Property Turkey",
+            "site_keywords" => "Buy real estate in Turkey, turkey real estate, Turkey properties",
+
+            "since_2001" => "Since 2001",
+
+            "site_description" => "Access 3,000+ Realestate on the most diversified real estate portfolio across the country. Today's Recommended Turkish Real estate",
+            "listings" => "Listings",
+            "info" => "Info",
+            "reviews" => "Reviews",
+            "news" => "News",
+            "site_phone" => "+90 212 215 00 22",
+            "emerging_global_realestate_goms" => "DISCOVER YOUR DREAM <br/>HOME IN BODRUM",
+            "stay_ahead_the_curve" => "1,000+ Sun-Soaked Villas & Apartments",
+            "scheduale_free_consultation" => "Schedule free consultation",
+
+            "separator_phrase_1" => "One-Stop Shop for Turkish Real Estate",
+            "separator_phrase_2" => "Near Blue Flag Beaches, Marinas",
+            "separator_phrase_3" => "Breathtaking Mediterranean Views",
+
+            "separator_button" => "FOR MANY MORE LISTINGS INQUIRE WITH US TODAY",
+
+            "offer1" => "<i>$2,100,000</i>Yalikavak, Bodrum<br/>4 Bed / 5 Bath<br/>320 m²",
+            "offer2" => "<i>$950,000</i>Gumusluk, Bodrum<br/>4 Bed / 5 Bath<br/>250 m²",
+            "offer3" => "<i>$1,200,000</i>Yalikavak, Bodrum<br/>4 Bed / 4 Bath<br/>230 m²",
+
+            "offer4" => "<i>$350,000</i>Gümüşlük, Bodrum<br/>2 Bed / 2 Bath<br/>125 m²",
+            "offer5" => "<i>$1,200,000</i>Yalikavak, Bodrum<br/>4 Bed / 4 Bath<br/>230 m²",
+            "offer6" => "<i>\$POA</i>Yalikavak, Bodrum<br/>6 Bed / 6 Bath<br/>550 m² (on 5,000 m²)",
+
+            "offer1_txt" => "The ‘U’ House in<br/>Gumusluk Village",
+            "offer2_txt" => "Jaw-Dropping Views from<br/>Breathtaking Villa",
+            "offer3_txt" => "Designer Homes with Panoramic<br/>Views of Gumusluk Bay",
+
+            "offer4_txt" => "Shoreside Beach Apartment<br/>in Gümüşlük, Bodrum",
+            "offer5_txt" => "Stylish Villa with Private Pool<br/>and Stunning Garden",
+            "offer6_txt" => "Creme de la Creme<br/>of Bodrum",
+
+            "inquire" => "Inquire",
+            "join_maillist" => "Join Our Mailing List and Stay Informed on All Things Turkish Real Estate.",
+            "name" => "Name",
+            "phone" => "Phone",
+            "email" => "Email",
+            "join" => "Join",
+            "featured_in" => "Featured in",
+            "partners_with" => "Partners with",
+
+            "rate1_title" => "Great Team. Very Helpful.",
+            "rate1_p" => "I had a great experience with this group. A big shoutout to Yunus and Faruk who helped me to understand the process of some investments. They are very knowledgeable and patient with answering all of my questions. I would recommend them to anyone.",
+            "rate1_writer" => "SOHEILA",
+            "rate1_date" => "November 3, 2022",
+
+            "rate2_title" => "I've Had a Very Successful Experience",
+            "rate2_p" => "Eren and Sabina were really helpful in offering me different properties according to my expectations, they took me to visit the properties, explained their context, made all the logistical arrangements and facilitated communication with concerned people and agencies. They offered all the support that I needed, even beyond the business subjects. I have felt their sincere concern to make the deal happen in the smoothest way. The legal team have also been very helpful and efficient. I am glad I’ve had Property Turkey as my agent.",
+            "rate2_writer" => "DEM GERD",
+            "rate2_date" => "September 3, 2022",
+
+            "rate3_title" => "Highly Recommend Property Turkey",
+            "rate3_p" => "I would highly recommend Property Turkey for foreigners looking to purchase properties in Turkey. Special shout out to Hazal who is incredibly helpful and patient in showing me various property options across Istanbul. She is truly amazing and a perfect example of the “client first” spirit.",
+            "rate3_writer" => "Henry Zhang",
+            "rate3_date" => "July 22, 2022",
+
+            "we_make_owning_property_easy" => "The Pearl of the Mediterranean, Antalya, Holds Many More Treasures Beyond Golden Rays",
+            "we_make_owning_property_easy_p" => "<p>Set in the heart of the Turkish Riviera, Antalya rules the Eastern Mediterranean. It is a unique modern metropolis littered with pockets of exquisite nature and ancient human history. It really is the capital of the region where many smaller beach towns shoot out from either side, up and down the coast.</p><p>Antalya, is famous for its blue flag beaches that stretch a mind blowing length in front of the city center. But the true beauty may lie at altitude. The unique geography of Antalya, allows its residents and visitors access to the sea and the summit within a single day. All that nature sound exhausting? Stay behind and rejuvenate in one of the hundred resort style residences in town.</p>",
+
+            "news1_title" => "Top 7 Boutique Hotels in Bodrum",
+            "news1_date" => date('F d, Y', strtotime('-21 DAYS')),
+            "news2_title" => "Bodrum with Children: Best Kids Activities in Bodrum",
+            "news2_date" => date('F d, Y', strtotime('-30 DAYS')),
+            "news3_title" => "Advantages to Growing Your Business In Turkey",
+            "news3_date" => date('F d, Y', strtotime('-30 DAYS')),
+
+            "read_more_news" => "READ MORE TURKISH<br /> REAL ESTATE NEWS",
+            "contact" => "Contact",
+            "learn_more_about_this_property" => "Learn more about this property, plus many more.",
+            "scheduale" => "Learn more",
+            "contact" => "Contact",
+            "read_more" => "Read more",
+            "sent-success" => "Thanks for Your Enquiry ..",
+            "close" => "Close",
+            "is-number-msg" => "Please enter a valid number",
+            "is-email-msg" => "Please enter a valid Email",
+            "is-integer-msg" => "Please enter a valid number",
+            "is-flaot-msg" => "Please enter a number with a comma",
+            "is-version-msg" => "Please enter a valid version number",
+            "is-password-msg" => "A password can contain numbers, letters and @ # $% ^ & * ()! _ -",
+            "is-paragraph-msg" => "Text is too short or longer than 255 characters",
+            "is-empty-msg" => "This field cannot be empty",
+            "is-selected-empty-msg" => "Please select an item from the list",
+            "is-phone-msg" => "Please enter a valid phone number",
+            "is-zipcode-msg" => "Code",
+            "is-4-digits-msg" => "Please enter 4 digits",
+            "wts_number" => "905051771838",
+
+            "inquire_header" => "Learn more about this property, Plus many more.",
+            "read_more_header" => "Stay current on all things Turkish real estate",
+            "contact_header" => "Curious about Turkish readl estate? You're in the right place.",
+            "scheduale_header" => "One of our senior advisors will be in touch for a candid chat with regards your requirements",
+            "country_code" => "Country code",
+            "copyrights" => "Copyright © 2023 Property Turkey Ltd. All Rights Reserved.",
+            "whatsapp_us" => "Whatsapp Us",
+            "contact_us" => "Contact Us",
+            "wts_number_link" => "https://bit.ly/3RXgJDD",
+            "form_link" => "https://bit.ly/3XtfjSK",
+        ],
+    ];
+
+    ?>
